907c971c2a0e6d2aeca88bf2126c093391cd0ccf
chelseashin <chaewonshin95@gmail.com>
2019-03-27 00:26:20 +0900

    190327 | algorithm_permutation & greedy

--- algorithm/computational_thinking/day18/05_베이비진.py
+++ algorithm/computational_thinking/day18/05_베이비진.py
@@ -1,58 +1,63 @@
 import sys
 sys.stdin = open("05.txt")
 
-def check_babyGin1(arr):
+def check_babyGin(c):
     global flag
     check = 0
-    if arr[0] == arr[1] and arr[1] == arr[2]: check += 1
 
-    if arr[0] + 1 == arr[1] and arr[1] + 1 == arr[2]: check += 1
+    if c % 2 == 0:
+        if p1[0] == p1[1] and p1[1] == p1[2]: check += 1
+        if p1[0] + 1 == p1[1] and p1[1] + 1 == p1[2]: check += 1
 
-    if check == 1:
+    elif c % 2 != 0:
+        if p2[0] == p2[1] and p2[1] == p2[2]: check += 1
+        if p2[0] + 1 == p2[1] and p2[1] + 1 == p2[2]: check += 1
+
+    if check != 0:
         flag = 1
         return
 
-def check_babyGin2(arr):
-    global flag
-    check = 0
-    if arr[0] == arr[1] and arr[1] == arr[2]: check += 1
-
-    if arr[0] + 1 == arr[1] and arr[1] + 1 == arr[2]: check += 1
-
-    if check == 1:
-        flag = 1
-        return
-
-def perm(n, k):
-    global arr
-    if k == n:
-        check_babyGin(arr)
-    else:
-        for i in range(k, n):
-            arr[k], arr[i] = arr[i], arr[k]
-            perm(n, k+1)
-            arr[k], arr[i] = arr[i], arr[k]
+def perm(n, k, c):
+    if c % 2 == 0:
+        if k == n:
+            check_babyGin(c)
+        else:
+            for i in range(k, n):
+                p1[k], p1[i] = p1[i], p1[k]
+                perm(n, k + 1, c)
+                p1[k], p1[i] = p1[i], p1[k]
+    elif c % 2 != 0:
+        if k == n:
+            check_babyGin(c)
+        else:
+            for i in range(k, n):
+                p2[k], p2[i] = p2[i], p2[k]
+                perm(n, k + 1, c)
+                p2[k], p2[i] = p2[i], p2[k]
 
 T = int(input())
 for tc in range(T):
     cards = list(map(int, input().split()))
     p1 = []
     p2 = []
-    flag = 0
     ans = 0
     for i in range(len(cards)):
         if i % 2 == 0:
             p1.append(cards[i])
             if len(p1) >= 3:
+                flag = 0
                 perm(len(p1), 0, i)
-                if flag == 1:    # and ans == 0:
+                if flag:
                     ans = 1
+                    break
         else:
             p2.append(cards[i])
             if len(p2) >= 3:
+                flag = 0
                 perm(len(p2), 0, i)
-                if flag == 1:    # ans == 0:
+                if flag == 1:
                     ans = 2
+                    break
 
     print("#{} {}".format(tc+1, ans))
 
