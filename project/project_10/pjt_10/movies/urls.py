3a2e819880871f31482ed0537c910eb5bceb2414
websvey1 <websvey1@gmail.com>
2019-04-24 18:30:59 +0900

    190424 | django_project_10

---
+++ project/project_10/pjt_10/movies/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from rest_framework_swagger.views import get_swagger_view
+from . import views
+
+urlpatterns = [
+    path('docs/', get_swagger_view()),
+    path('scores/<int:score_pk>/', views.score_update_and_delete),
+    path('scores/<int:score_pk>/', views.score_update_and_delete),
+    path('movies/<int:movie_pk>/scores/', views.score_create),
+    path('movies/<int:movie_pk>/', views.movie_detail),
+    path('movies/', views.movie_list),
+    path('genres/<int:genre_pk>/', views.genre_detail),
+    path('genres/', views.genre_list),
+    ]
